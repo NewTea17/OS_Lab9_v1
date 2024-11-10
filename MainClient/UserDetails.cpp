d5995bfc9e3194b8bb503b63f8c92d64de214249
Solomia Bodnar <bodnar.solomia.8a@gmail.com>
2024-11-10 14:49:48 +0200

    Errors fixed

--- MainClient/UserDetails.cpp
+++ MainClient/UserDetails.cpp
@@ -4,21 +4,21 @@ UserDetails::UserDetails()
     : name("Unknown user name"), email("Unknown user email")
 { }
 
-UserDetails::UserDetails(String^ name, String^ email)
+UserDetails::UserDetails(System::String^ name, System::String^ email)
     : name(name), email(email)
 { }
 
 // Метод для перевірки валідності облікових даних
 bool UserDetails::isValid() {
     // Перевіряємо, що ім'я не є порожнім
-    if (String::IsNullOrEmpty(name)) {
+    if (System::String::IsNullOrEmpty(name)) {
         return false;
     }
 
     // Перевіряємо, що email не є порожнім і відповідає простому формату email
-    String^ emailPattern = R"((\w+)(\.|_)?(\w*)@(\w+)(\.(\w+))+)";
+    System::String^ emailPattern = R"((\w+)(\.|_)?(\w*)@(\w+)(\.(\w+))+)";
 
-    if (String::IsNullOrEmpty(email) || !Regex::IsMatch(email, emailPattern)) {
+    if (System::String::IsNullOrEmpty(email) || !Regex::IsMatch(email, emailPattern)) {
         return false;
     }
 
