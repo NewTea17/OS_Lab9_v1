1d2a0f7de96398ff45fb4708764b27af58aa2865
Yaroslav Guz <aroslavguz@gmail.com>
2024-11-10 11:51:46 +0200

    Create test user details form. It will be changed

--- MainClient/UserDetailsForm.cpp
+++ MainClient/UserDetailsForm.cpp
@@ -15,18 +15,67 @@ MainClient::UserDetailsForm::~UserDetailsForm()
 
 void MainClient::UserDetailsForm::InitializeComponent(void)
 {
+	this->usernameTextBox = (gcnew System::Windows::Forms::TextBox());
+	this->userEmailTextBox = (gcnew System::Windows::Forms::TextBox());
+	this->loginLanel = (gcnew System::Windows::Forms::Label());
+	this->loginBtn = (gcnew System::Windows::Forms::Button());
 	this->SuspendLayout();
+	// 
+	// usernameTextBox
+	// 
+	this->usernameTextBox->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+		static_cast<System::Byte>(0)));
+	this->usernameTextBox->Location = System::Drawing::Point(362, 162);
+	this->usernameTextBox->Name = L"usernameTextBox";
+	this->usernameTextBox->Size = System::Drawing::Size(350, 34);
+	this->usernameTextBox->TabIndex = 0;
+	// 
+	// userEmailTextBox
+	// 
+	this->userEmailTextBox->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+		static_cast<System::Byte>(0)));
+	this->userEmailTextBox->Location = System::Drawing::Point(362, 222);
+	this->userEmailTextBox->Name = L"userEmailTextBox";
+	this->userEmailTextBox->Size = System::Drawing::Size(350, 34);
+	this->userEmailTextBox->TabIndex = 1;
+	// 
+	// loginLanel
+	// 
+	this->loginLanel->AutoSize = true;
+	this->loginLanel->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 16, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+		static_cast<System::Byte>(0)));
+	this->loginLanel->Location = System::Drawing::Point(493, 85);
+	this->loginLanel->Name = L"loginLanel";
+	this->loginLanel->Size = System::Drawing::Size(80, 31);
+	this->loginLanel->TabIndex = 2;
+	this->loginLanel->Text = L"Login";
+	// 
+	// loginBtn
+	// 
+	this->loginBtn->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+		static_cast<System::Byte>(0)));
+	this->loginBtn->Location = System::Drawing::Point(467, 276);
+	this->loginBtn->Name = L"loginBtn";
+	this->loginBtn->Size = System::Drawing::Size(142, 42);
+	this->loginBtn->TabIndex = 3;
+	this->loginBtn->Text = L"Save";
+	this->loginBtn->UseVisualStyleBackColor = true;
 	// 
 	// UserDetailsForm
 	// 
 	this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 	this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 	this->ClientSize = System::Drawing::Size(1074, 518);
+	this->Controls->Add(this->loginBtn);
+	this->Controls->Add(this->loginLanel);
+	this->Controls->Add(this->userEmailTextBox);
+	this->Controls->Add(this->usernameTextBox);
 	this->Name = L"UserDetailsForm";
 	this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
 	this->Text = L"UserDetailsForm";
 	this->Load += gcnew System::EventHandler(this, &UserDetailsForm::UserDetailsForm_Load);
 	this->ResumeLayout(false);
+	this->PerformLayout();
 
 }
 
--- MainClient/UserDetailsForm.h
+++ MainClient/UserDetailsForm.h
@@ -15,9 +15,14 @@ namespace MainClient {
 
 	protected:
 		~UserDetailsForm();
+	private: System::Windows::Forms::TextBox^ usernameTextBox;
+	protected:
+	private: System::Windows::Forms::TextBox^ userEmailTextBox;
+	private: System::Windows::Forms::Label^ loginLanel;
+	private: System::Windows::Forms::Button^ loginBtn;
 
 	private:
-		System::ComponentModel::Container ^components;
+		System::ComponentModel::Container^ components;
 
 		void InitializeComponent(void);
 
